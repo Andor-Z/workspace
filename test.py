78dad3ab17fe5db3fd03a9d4b6a316de4096eb13
Andor-Z <zyfsta@outlook.com>
2015-11-10 17:31:03 +0800

    +

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
-class student(object):
+import os
 
-	def __init__(self, name, score):
-		self.name = name
-		self.score = score
-
-	def print_score(self):
-		print('%s:%s' %(self.name, self.score))
+[x for x in os.listdir('.') if os.path.isfile(x) and os.path.splitext(x)[1] == '.py']
 
 
-bart = student('Bart Simpson', 59)
 
-bart.print_score()
+
+
+
+
+
+
+
+
+
+
+
